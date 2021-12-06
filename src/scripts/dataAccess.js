3b0d089dc447b1a5c217895820482d6ebee8e89f
sorachung <sorachung.src@gmail.com>
2021-12-06 15:33:50 -0600

    setup fetch for database.json

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,17 @@
 const applicationState = {
+  requests: [],
+};
 
-}
+const API = "http://localhost:8088";
 
+export const fetchRequests = () => {
+  return fetch(`${API}/requests`)
+    .then((response) => response.json())
+    .then((serviceRequests) => {
+      // Store the external state in application state
+      applicationState.requests = serviceRequests;
+    });
+};
+
+export const getRequests = () =>
+  applicationState.requests.map((request) => ({ ...request }));
